0897c550d51298be2a23316c84a6b98f07098d25
Rainier102625 <149060203+Rainier102625@users.noreply.github.com>
2024-05-01 05:22:59 +0000

    lesson-13_end_2425409

--- lesson-13/main.js
+++ lesson-13/main.js
@@ -6,11 +6,47 @@ const port = 3000,
   layouts = require("express-ejs-layouts"),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  app = express();
-  // @TODO: 몽고DB 모듈의 요청
+  app = express(),
+  MongoDB = require('mongodb').MongoClient,
+  dbURL = "mongodb+srv://eodnjs9906:0cWiPlpoo7TceYzf@ut-node.rkbl8lc.mongodb.net/?retryWrites=true&w=majority&appName=UT-node",
+  dbName = "ut-node";
 
-// @TODO: 로컬 MongoDB 데이터베이스 서버 연결 설정
+// @TODO: 몽고DB 모듈의 요청
 
+
+
+// @TODO: atlas MongoDB 데이터베이스 서버 연결 설정
+MongoDB.connect(dbURL,(error, client) => {
+  if (error)
+  throw error;
+
+  let db = client.db(dbName);
+  db.collection("contacts")
+  .find()
+  .toArray((error, data) => {
+    if(error) throw error;
+    console.log(data);
+  })
+  db.collection("contacts")
+  .insertOne({
+    name: "Psy",
+    jod: "singer",
+    from: "Korea"
+  }, (error, result) => {
+    if(error)
+    throw error;
+  console.log(result);
+  })
+  
+})
+.then(() => {
+  console.log("DB connected!!");
+})
+.catch((error) => {
+  console.log("DB connecttion FAILED");
+  console.log(error);
+  process.exit(1);
+});
 
 app.set("port", process.env.PORT || port);
 app.set("view engine", "ejs");
